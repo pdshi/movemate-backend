c779cd70ac72fc624dcb489a1f02ee7e7fa113f8
alyafitria <alyafitria947@gmail.com>
2023-05-22 05:47:05 +0700

    second commit - rev basic register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,17 +4,11 @@ const bcrypt = require('bcrypt');
 
 const register = async (req, res) => {
 
-    const { display_name, email, password, photo_url, provider, key } = req.body;
+    const { email, password, provider } = req.body;
 
-    //check display_name and password if they are empty
-    if (!display_name || !email || !password) {
+    //check email and password if they are empty
+    if ( !email || !password) {
         return res.status(400).json({ success: false, message: 'display name, email and password are required' });
-    }
-
-    // Validate the display_name input
-    const display_nameRegex = /^\S+$/;
-    if (!display_nameRegex.test(display_name)) {
-        return res.status(400).json({ success: false, message: 'Invalid display name' });
     }
 
     // Validate the email input
@@ -22,15 +16,12 @@ const register = async (req, res) => {
     if (!emailRegex.test(email)) {
         return res.status(400).json({ success: false, message: 'Invalid email' });
     }
+    let display_name = email.split("@")[0];
 
     // Validate the password input
     const passwordRegex = /^.{8,}$/;
     if (!passwordRegex.test(password)) {
         return res.status(400).json({ success: false, message: 'Invalid password' });
-    }
-    
-    if (key !== process.env.REGISTRATION_KEY) {
-        return res.status(401).json({ success: false, message: 'Invalid key' });
     }
 
     try {
@@ -41,11 +32,14 @@ const register = async (req, res) => {
             return res.status(409).json({ success: false, message: 'User already exists' });
         }
 
-        // Created
+        // Create provider and key
+        const provider = 'movemate.com';
+        const key = process.env.REGISTRATION_KEY;
+
         // Hash the password and create a new admin
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = await User.create({ display_name, email, password: hashedPassword, photo_url, provider });
+        const newUser = await User.create({ display_name, email, password: hashedPassword, provider, key });
 
         res.status(201).json({ success: true, message: 'User created successfully' + newUser.display_name });
     } catch (err) {
