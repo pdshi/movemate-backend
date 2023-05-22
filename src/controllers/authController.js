f3631befc5b78063d0e771a2f6a5d229580bf378
alyafitria <alyafitria947@gmail.com>
2023-05-22 16:54:44 +0700

    third commit - rev basic register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,11 +4,11 @@ const bcrypt = require('bcrypt');
 
 const register = async (req, res) => {
 
-    const { email, password, provider } = req.body;
+    const { email, password, key } = req.body;
 
     //check email and password if they are empty
     if ( !email || !password) {
-        return res.status(400).json({ success: false, message: 'display name, email and password are required' });
+        return res.status(400).json({ success: false, message: 'Email and Password are required' });
     }
 
     // Validate the email input
@@ -24,9 +24,13 @@ const register = async (req, res) => {
         return res.status(400).json({ success: false, message: 'Invalid password' });
     }
 
+    if (key !== process.env.API_KEY) {
+        return res.status(401).json({ success: false, message: 'Invalid key' });
+    }
+
     try {
         // Check if the admin already exists
-        const existingUser = await User.findOne({ where: { display_name } });
+        const existingUser = await User.findOne({ where: { email } });
 
         if (existingUser) {
             return res.status(409).json({ success: false, message: 'User already exists' });
@@ -34,12 +38,11 @@ const register = async (req, res) => {
 
         // Create provider and key
         const provider = 'movemate.com';
-        const key = process.env.REGISTRATION_KEY;
 
         // Hash the password and create a new admin
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = await User.create({ display_name, email, password: hashedPassword, provider, key });
+        const newUser = await User.create({ display_name, email, password: hashedPassword, provider });
 
         res.status(201).json({ success: true, message: 'User created successfully' + newUser.display_name });
     } catch (err) {
