f63207a9356af9968155c0af1c21a19606802423
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2023-06-02 21:19:00 +0700

    feat: edit user data

--- src/controllers/userDataController.js
+++ src/controllers/userDataController.js
@@ -36,3 +36,33 @@ const getUserData = async (req, res) => {
     return res.status(200).json({ status: true, message: 'User data found', data: userData });
 
 }
+
+const editUserData = async (req, res) => {
+
+    const { gender, age, height, weight, goal, goal_weight, spare_days } = req.body;
+    const user_id = req.decodedToken.user_id;
+
+    try {
+        let userData = await UserData.findOne({ where: { user_id } });
+
+        if (!userData) {
+            return res.status(404).json({ success: false, message: 'User data not found' });
+        }
+
+        userData.gender = gender ?? userData.gender;
+        userData.age = age ?? userData.age;
+        userData.height = height ?? userData.height;
+        userData.weight = weight ?? userData.weight;
+        userData.goal = goal ?? userData.goal;
+        userData.goal_weight = goal_weight ?? userData.goal_weight;
+        userData.spare_days = spare_days ?? userData.spare_days;
+
+        await userData.save();
+
+        return res.status(200).json({ status: true, message: 'User data updated successfully' });
+    } catch (error) {
+        // Handle any errors that occur during the process
+        console.error(error);
+        return res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+};
