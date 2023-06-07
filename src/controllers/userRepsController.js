4f8929d4ccdd39835255e7bc10d3bec3423c7feb
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2023-06-08 00:42:04 +0700

    fix: user reps controller

--- src/controllers/userRepsController.js
+++ src/controllers/userRepsController.js
@@ -2,23 +2,14 @@ const UserReps = require('../models/userRepsModel');
 
 const inputUserReps = async (req, res) => {
 
-    const { type, reps } = req.body;
+    const { type, reps, sets, date, start, end } = req.body;
     const user_id = req.decodedToken;
 
     try {
 
-        let userReps = await UserReps.findOne({ where: { user_id, type } });
-        if (userReps) {
+        await UserReps.create({ user_id, type, reps, sets, date, start, end });
 
-            return res.status(409).json({ success: false, message: 'User reps data already exists' });
-
-        } else {
-
-            userReps = await UserData.create({ user_id, type, reps });
-
-        }
-
-        return res.status(200).json({ success: true, message: 'User reps created successfully' });
+        return res.status(200).json({ success: true, message: 'User reps inserted successfully' });
 
     } catch (error) {
 
@@ -31,16 +22,40 @@ const inputUserReps = async (req, res) => {
 
 const getUserReps = async (req, res) => {
 
+    const { current_date } = req.body;
     const { user_id } = req.decodedToken;
 
     try {
 
-        const userReps = await UserReps.findAll({ where: { user_id } });
+        let userReps = await UserReps.findOne({ where: { user_id, date: new Date(current_date) } });
         if (!userReps) {
 
             return res.status(404).json({ success: false, message: 'User reps not found' });
 
         }
+
+        userReps = await UserReps.findAll({ where: { user_id, created_at: userReps.created_at } });
+
+
+        // let addDate = 1;
+        // // infinite while loop, exit with break
+        // while (true) {
+
+        //     // new date and + addDate
+        //     let newDate = new Date(current_date);
+        //     newDate.setDate(newDate.getDate() + addDate);
+
+        //     let nextUserReps = await UserReps.findAll({ where: { user_id, date: newDate } });
+        //     if (!nextUserReps) break;
+
+        //     // add nextUserReps to userReps
+        //     userReps = [...userReps, ...nextUserReps];
+
+        //     addDate++;
+
+        // }
+
+
 
         return res.status(200).json({ success: true, message: 'User reps found', data: userReps });
 
@@ -53,43 +68,43 @@ const getUserReps = async (req, res) => {
     }
 };
 
-const editUserReps = async (req, res) => {
+// const editUserReps = async (req, res) => {
 
-    const { type, reps } = req.body;
-    const user_id = req.decodedToken;
+//     const { type, reps } = req.body;
+//     const user_id = req.decodedToken;
 
-    if (!type || !reps) {
+//     if (!type || !reps) {
 
-        return res.status(400).json({ success: false, message: 'Please provide all required fields' });
+//         return res.status(400).json({ success: false, message: 'Please provide all required fields' });
 
-    };
+//     };
 
-    try {
+//     try {
 
-        let userReps = await UserReps.findOne({ where: { user_id, type } });
-        if (!userReps) {
+//         let userReps = await UserReps.findOne({ where: { user_id, type } });
+//         if (!userReps) {
 
-            return res.status(404).json({ success: false, message: 'User reps data not found' });
+//             return res.status(404).json({ success: false, message: 'User reps data not found' });
 
-        }
+//         }
 
-        userReps.reps = reps || userReps.reps;
+//         userReps.reps = reps || userReps.reps;
 
-        await userReps.save();
+//         await userReps.save();
 
-        return res.status(200).json({ success: true, message: 'User reps data updated successfully' });
+//         return res.status(200).json({ success: true, message: 'User reps data updated successfully' });
 
-    } catch (error) {
+//     } catch (error) {
 
-        // Handle any errors that occur during the process
-        console.error(error);
-        return res.status(500).json({ success: false, message: error.message });
+//         // Handle any errors that occur during the process
+//         console.error(error);
+//         return res.status(500).json({ success: false, message: error.message });
 
-    }
-};
+//     }
+// };
 
 module.exports = {
     inputUserReps,
     getUserReps,
-    editUserReps
+    //editUserReps
 };
